760700a12ee1e7ce8cc419249c570952d7d4333a
Nokutenda Gift Remwa <gnremwa@gmail.com>
2026-03-18 14:36:02 +0200

    Add box predictor to our model: model.py

--- model.py
+++ model.py
@@ -11,6 +11,8 @@ def build_model(backbone: str, num_classes: int):
     else:
         model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn(pretrained=True, weights=weights num_classes=num_classes)
         weights = torchvision.models.detection.FasterRCNN_MobileNet_V3_Large_FPN_Weights.DEFAULT
+    in_feature = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_feature, num_classes = num_classes)
 
     return model
 
