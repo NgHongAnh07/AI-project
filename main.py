f563dfd21acce6334d28202e679dfa55e0ba8403
github file <sc15531@student.samk.fi>
2026-03-23 10:25:32 +0200

    Add training model: main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from args import get_args
 import pandas as pd
 import os
-import torch 
 from dataset import ObjDetectionDataset
 from torch.utils.data import DataLoader
 from model import build_model
-from trainer import train_model 
 
 def collate(batch):
     images, targets = zip(*batch)
@@ -13,30 +11,30 @@ def collate(batch):
 
 def main():
     args = get_args()
-    
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    print(f"--> Using device: {device}")
-
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+   
+    # 1. Read the dataframes
     train_df = pd.read_csv(os.path.join(args.csv_dir, 'train_df.csv'))
-    val_df = pd.read_csv(os.path.join(args.csv_dir, 'val_df.csv'))
+    val_df =  pd.read_csv(os.path.join(args.csv_dir, 'val_df.csv'))
 
+    # 2. Prepare datasets
     train_dataset = ObjDetectionDataset(train_df)
     val_dataset = ObjDetectionDataset(val_df)
 
-    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collate)
-    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=collate)
+    # 3. Create data loaders
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collate, num_worker=0, pin_memory=torch.cuda.is_available())
+    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=collate, num_worker=0, pin_memory=torch.cuda.is_available())
 
-    model = build_model(args.backbone, args.num_classes)
+    # images, targets = next(iter(train_loader))
+
+    #4 Initializing the model
+    model = build_model(args.backbone, args.num_classes + 1)
+
+    # 5. Train the model
+    train_model(model, train_loader, val_loader, device)
+
     
-    print(f"--> Starting training for {args.epochs} epochs...")
-    train_model(
-        model=model,
-        train_loader=train_loader,
-        val_loader=val_loader,
-        device=device
-    )
-
-    print("--> Assignment complete! Check the 'outputs' folder for best_model.pth")
+    print()
 
 if __name__ == "__main__":
     main()
